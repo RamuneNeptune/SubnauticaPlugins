77e97e3623316cd3ee6ce66eeaeedfb179f09c1c
ramennoodlesxv <94365980+RamuneNeptune@users.noreply.github.com>
2023-02-18 15:49:32 +1300

    References & Main.cs

--- SeamothRadarModule/Main.cs
+++ SeamothRadarModule/Main.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using BepInEx.Logging;
+using BepInEx;
+using HarmonyLib;
 
-namespace SeamothRadarModule
+namespace Ramune.SeamothRadarModule
 {
-    public class Class1
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    [BepInProcess("Subnautica.exe")]
+    public class SeamothRadarModule : BaseUnityPlugin
     {
+        private const string myGUID = "com.ramune.SeamothRadarModule";
+        private const string pluginName = "Seamoth Radar Module";
+        private const string versionString = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+        public static ManualLogSource logger;
+
+        public void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "has been loaded! (yay)");
+            logger = Logger;
+
+            StartCoroutine(RamuneLib.Main.Sprite.GetSubmodicaSprites());
+        }
     }
 }
--- SeamothTurboModule/Main.cs
+++ SeamothTurboModule/Main.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using BepInEx.Logging;
+using BepInEx;
+using HarmonyLib;
 
-namespace SeamothTurboModule
+namespace Ramune.SeamothTurboModule
 {
-    public class Class1
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    [BepInProcess("Subnautica.exe")]
+    public class SeamothTurboModule : BaseUnityPlugin
     {
+        private const string myGUID = "com.ramune.SeamothTurboModule";
+        private const string pluginName = "Seamoth Turbo Module";
+        private const string versionString = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+        public static ManualLogSource logger;
+
+        public void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "has been loaded! (yay)");
+            logger = Logger;
+
+            StartCoroutine(RamuneLib.Main.Sprite.GetSubmodicaSprites());
+        }
     }
 }
